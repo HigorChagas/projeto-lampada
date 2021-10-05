6b688323e86759c116451b2ec129a3ee7a172739
Higor Chagas <higornchagas@gmail.com>
2021-10-05 17:02:39 -0300

    Refatorando código

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
-const lampada = document.getElementById('lampada');
-const botaoAcender = document.getElementById('ligar');
-const botaoApagar = document.getElementById('desligar');
+function lampadaReal() {
 
-const audioQuebraLampada = new Audio('lampada-quebrando.mp3');
-const audioLigaLampada = new Audio('interruptor.mp3');
+    const lampada = document.getElementById('lampada');
+    const botaoAcender = document.getElementById('ligar');
+    const botaoApagar = document.getElementById('desligar');
 
-lampada.addEventListener('mouseover', acendeLampada);
-lampada.addEventListener('mouseout', apagaLampada);
-lampada.addEventListener('dblclick', quebraLampada);
+    const audioQuebraLampada = new Audio('lampada-quebrando.mp3');
+    const audioLigaLampada = new Audio('interruptor.mp3');
 
-botaoAcender.addEventListener('click', acendeLampada)
-botaoApagar.addEventListener('click', apagaLampada)
+    lampada.addEventListener('mouseover', acendeLampada);
+    lampada.addEventListener('mouseout', apagaLampada);
+    lampada.addEventListener('dblclick', quebraLampada);
 
-function acendeLampada() {
-    lampada.src = './img/ligada.jpg'
-    audioLigaLampada.play();
-    
+    botaoAcender.addEventListener('click', acendeLampada)
+    botaoApagar.addEventListener('click', apagaLampada)
+
+    function acendeLampada() {
+        lampada.src = './img/ligada.jpg'
+        audioLigaLampada.play();
+        
+    }
+
+    function apagaLampada() {
+        lampada.src = './img/desligada.jpg'
+        audioLigaLampada.play()
+    }
+
+    function quebraLampada() {
+        lampada.src = './img/quebrada.jpg'
+        lampada.removeEventListener('mouseover', acendeLampada);
+        lampada.removeEventListener('mouseout', apagaLampada);
+        botaoAcender.removeEventListener('click', acendeLampada);
+        botaoApagar.removeEventListener('click', apagaLampada);
+        audioQuebraLampada.play()
+    }
 }
 
-function apagaLampada() {
-    lampada.src = './img/desligada.jpg'
-    audioLigaLampada.play()
-}
-
-function quebraLampada() {
-    lampada.src = './img/quebrada.jpg'
-    lampada.removeEventListener('mouseover', acendeLampada);
-    lampada.removeEventListener('mouseout', apagaLampada);
-    botaoAcender.removeEventListener('click', acendeLampada);
-    botaoApagar.removeEventListener('click', apagaLampada);
-    audioQuebraLampada.play()
-}
+lampadaReal();
